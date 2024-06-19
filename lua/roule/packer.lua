a05b90e0f9997d00e577f99c21c4fd8f5b624f6c
Delioos <htz_j@protonmail.com>
2024-06-19 23:22:33 +0200

    fix: uniform colorscheme (between file and popups)

--- lua/roule/packer.lua
+++ lua/roule/packer.lua
@@ -13,8 +13,9 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  -- install the theme
+  -- install themes
   use { "ellisonleao/gruvbox.nvim" }
+  use { "catppuccin/nvim", as = "catppuccin" }
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
