348e05d4a62f68fbf58a470d386a35422401541f
Delioos <htz_j@protonmail.com>
2024-06-21 10:54:45 +0200

    feat(shortcuts): add harpoon clear all and term shortcuts

--- lua/roule/remap.lua
+++ lua/roule/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("i", "jk", "<Esc>")
+
+vim.api.nvim_set_keymap('n', '<leader>t', ':20split +terminal<CR>', { noremap = true, silent = true })
